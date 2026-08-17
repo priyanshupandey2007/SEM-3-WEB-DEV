0a2a7d3d52271cfac30d4a21fc5dad99c721c143
priyanshupandey2007 <priyanshupandey2864@gmail.com>
2026-08-17 15:53:41 +0530

    updated

---
+++ UNIT-2/WEEK-1/day-1/1.js
@@ -0,0 +1,21 @@
+// step-1 import expressjs
+const express = require("express");
+
+// step-2 create a application through express.js
+const app = express();
+
+// step-4 create API/routing
+app.get("/", (req,res) => {
+    res.end("home page");
+});
+
+app.get("/about", (req,res) => {
+    res.send("about page");
+});
+
+// step-3 listen/ run application on port adderess
+
+app.listen(8080, () => {
+    console.log("server started")
+})
+
